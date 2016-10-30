0334694173f0b9b8abf8aaab41d564036780a17a
Justus Ranvier <justusranvier@riseup.net>
2016-10-30 00:17:56 -0500

    New command: acknowledgenotice

---
+++ src/cli/commands/CmdAcknowledgeNotice.cpp
@@ -0,0 +1,93 @@
+/************************************************************
+ *
+ *                 OPEN TRANSACTIONS
+ *
+ *       Financial Cryptography and Digital Cash
+ *       Library, Protocol, API, Server, CLI, GUI
+ *
+ *       -- Anonymous Numbered Accounts.
+ *       -- Untraceable Digital Cash.
+ *       -- Triple-Signed Receipts.
+ *       -- Cheques, Vouchers, Transfers, Inboxes.
+ *       -- Basket Currencies, Markets, Payment Plans.
+ *       -- Signed, XML, Ricardian-style Contracts.
+ *       -- Scripted smart contracts.
+ *
+ *  EMAIL:
+ *  fellowtraveler@opentransactions.org
+ *
+ *  WEBSITE:
+ *  http://www.opentransactions.org/
+ *
+ *  -----------------------------------------------------
+ *
+ *   LICENSE:
+ *   This Source Code Form is subject to the terms of the
+ *   Mozilla Public License, v. 2.0. If a copy of the MPL
+ *   was not distributed with this file, You can obtain one
+ *   at http://mozilla.org/MPL/2.0/.
+ *
+ *   DISCLAIMER:
+ *   This program is distributed in the hope that it will
+ *   be useful, but WITHOUT ANY WARRANTY; without even the
+ *   implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *   PARTICULAR PURPOSE.  See the Mozilla Public License
+ *   for more details.
+ *
+ ************************************************************/
+
+#include "CmdAcknowledgeNotice.hpp"
+
+#include <opentxs/core/Version.hpp>
+#include <opentxs/client/OT_ME.hpp>
+
+namespace opentxs {
+
+CmdAcknowledgeNotice::CmdAcknowledgeNotice()
+{
+    command = "acknowledgenotice";
+    args[0] = "--server <server>";
+    args[1] = "--mynym <nym>";
+    args[2] = "--hisnym <nym>";
+    args[3] = "--mypurse ID for the peer notice being acknowledged";
+    category = catOtherUsers;
+    help = "Acknowledge receipt of a peer notice";
+}
+
+CmdAcknowledgeNotice::~CmdAcknowledgeNotice()
+{
+}
+
+std::int32_t CmdAcknowledgeNotice::runWithOptions()
+{
+    return run(
+        getOption("server"),
+        getOption("mynym"),
+        getOption("hisnym"),
+        getOption("mypurse"));
+}
+
+std::int32_t CmdAcknowledgeNotice::run(
+    std::string server,
+    std::string mynym,
+    std::string hisnym,
+    std::string mypurse)
+{
+    if (!checkServer("server", server)) {
+        return -1;
+    }
+
+    if (!checkNym("mynym", mynym)) {
+        return -1;
+    }
+
+    if (!checkNym("hisnym", hisnym)) {
+        return -1;
+    }
+
+    OT_ME ot_me;
+    std::string response = ot_me.acknowledge_notice(
+        server, mynym, hisnym, mypurse, true);
+    return processResponse(response, "acknowledge notice");
+}
+} // namespace opentxs

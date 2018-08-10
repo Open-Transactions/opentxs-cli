1c71568927da19c6794e029f28fa8587d66bc62e
Justus Ranvier <justus@stashcrypto.com>
2018-08-10 11:45:05 -0500

    New command: startserver

---
+++ src/cli/commands/CmdStartServer.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2018 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "CmdStartServer.hpp"
+
+#include <opentxs/opentxs.hpp>
+
+#include <cstdint>
+#include <string>
+
+using namespace opentxs;
+
+CmdStartServer::CmdStartServer()
+{
+    command = "startserver";
+    args[0] = "--instance <instance number>";
+    category = catMisc;
+    help = "Start a server for testing";
+}
+
+std::int32_t CmdStartServer::runWithOptions()
+{
+    return run(getOption("instance"));
+}
+
+std::int32_t CmdStartServer::run(const std::string& instance)
+{
+    const auto value = opentxs::String::Factory(instance)->ToLong();
+    auto& server =
+        OT::App().StartServer({{"inproc", {std::to_string(value)}}}, value);
+    otErr << "Started server " << server.Instance() << std::endl;
+
+    return 0;
+}
